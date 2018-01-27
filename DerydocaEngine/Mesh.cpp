2ee5159b184c732b21a0e67d17d1ecfd7bfaee6e
Derrick Canfield <derrick.canfield@gmail.com>
2018-01-26 16:52:28 -0800

    Converting mesh processing to use assimp
    I am currently phasing out the current model loader for the assimp loader because of the greater flexibility it provides.

--- DerydocaEngine/Mesh.cpp
+++ DerydocaEngine/Mesh.cpp
@@ -1,5 +1,9 @@
 #include "Mesh.h"
 
+#include "assimp\cimport.h"
+#include "assimp\scene.h"
+#include "assimp\postprocess.h"
+
 Mesh::Mesh() {
 
 }
@@ -26,8 +30,87 @@ Mesh::Mesh(Vertex* vertices, unsigned int numVertices, unsigned int* indices, un
 
 Mesh::Mesh(const std::string& fileName)
 {
-	IndexedModel model = OBJModel(fileName).ToIndexedModel();
-	InitMesh(model);
+	auto aiModel = aiImportFile(fileName.c_str(), aiProcessPreset_TargetRealtime_MaxQuality);
+
+	int meshIndex = 0;
+	int uvIndex = 0;
+
+	if (meshIndex >= aiModel->mNumMeshes)
+	{
+		return;
+	}
+
+	aiMesh* mesh = aiModel->mMeshes[meshIndex];
+
+	m_numIndices = mesh->mNumFaces * 3;
+
+	glGenVertexArrays(1, &m_vertexArrayObject);
+	glBindVertexArray(m_vertexArrayObject);
+
+	glGenBuffers(NUM_BUFFERS, m_vertexArrayBuffers);
+
+	// Initialize the vert positions
+	if (mesh->HasPositions())
+	{
+		float * verts = new float[mesh->mNumVertices * 3];
+		for (int i = 0; i < mesh->mNumVertices; i++)
+		{
+			verts[i * 3 + 0] = mesh->mVertices[i].x;
+			verts[i * 3 + 1] = mesh->mVertices[i].y;
+			verts[i * 3 + 2] = mesh->mVertices[i].z;
+		}
+		glBindBuffer(GL_ARRAY_BUFFER, m_vertexArrayBuffers[POSITION_VB]);
+		glBufferData(GL_ARRAY_BUFFER, 3 * mesh->mNumVertices * sizeof(GLfloat), verts, GL_STATIC_DRAW);
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	}
+
+	// Initialize the texture coordinates buffer
+	if (mesh->HasTextureCoords(uvIndex))
+	{
+		float * texCoords = new float[mesh->mNumVertices * 2];
+		for (int i = 0; i < mesh->mNumVertices; i++)
+		{
+			texCoords[i * 2 + 0] = mesh->mTextureCoords[uvIndex][i].x;
+			texCoords[i * 2 + 1] = mesh->mTextureCoords[uvIndex][i].y;
+		}
+		glBindBuffer(GL_ARRAY_BUFFER, m_vertexArrayBuffers[TEXCOORD_VB]);
+		glBufferData(GL_ARRAY_BUFFER, 2 * mesh->mNumVertices * 2 * sizeof(GLfloat), texCoords, GL_STATIC_DRAW);
+		glEnableVertexAttribArray(1);
+		glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, 0);
+	}
+
+	// Initialize the normals buffer
+	if (mesh->HasNormals())
+	{
+		float * normals = new float[mesh->mNumVertices * 3];
+		for (int i = 0; i < mesh->mNumVertices; i++)
+		{
+			normals[i * 3 + 0] = mesh->mNormals[i].x;
+			normals[i * 3 + 1] = mesh->mNormals[i].y;
+			normals[i * 3 + 2] = mesh->mNormals[i].z;
+		}
+		glBindBuffer(GL_ARRAY_BUFFER, m_vertexArrayBuffers[NORMAL_VB]);
+		glBufferData(GL_ARRAY_BUFFER, 3 * mesh->mNumVertices * sizeof(GLfloat), normals, GL_STATIC_DRAW);
+		glEnableVertexAttribArray(2);
+		glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	}
+
+	// Initialize the indices buffer
+	if (mesh->HasFaces())
+	{
+		unsigned int *indices = new unsigned int[m_numIndices];
+		for (unsigned int i = 0; i < mesh->mNumFaces; i++)
+		{
+			indices[i * 3 + 0] = mesh->mFaces[i].mIndices[0];
+			indices[i * 3 + 1] = mesh->mFaces[i].mIndices[1];
+			indices[i * 3 + 2] = mesh->mFaces[i].mIndices[2];
+		}
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_vertexArrayBuffers[INDEX_VB]);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, m_numIndices * sizeof(GLuint), indices, GL_STATIC_DRAW);
+	}
+
+	glBindVertexArray(0);
 }
 
 Mesh::~Mesh()
