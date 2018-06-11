30d1482dd18b533febf28c416f4f3f9a06cd6f45
Derrick Canfield <derrick.canfield@gmail.com>
2018-06-11 15:21:35 -0700

    Created disintegration effect
    - Created disintegration effect shader
    - Created level and material to demonstrate the shader

---
+++ exampleProject/shader_cookbook/chapter_8/8_4_disintegrationEffect/shaders/disintegrationEffect.fs
@@ -0,0 +1,64 @@
+#version 400
+
+struct LightInfo
+{
+    vec4 Position;
+    vec4 Intensity;
+    mat4 ShadowMatrix;
+    float ShadowSoftness;
+};
+uniform LightInfo Lights[10];
+uniform int LightCount;
+
+struct MaterialInfo {
+    vec4 Kd;
+    vec4 Ka;
+    vec4 Ks;
+    float Shininess;
+    sampler2D AOTex;
+    sampler2D DiffuseTex;
+};
+uniform MaterialInfo Material;
+
+uniform sampler2D NoiseTexture;
+
+in vec3 Position;
+in vec3 Normal;
+in vec2 TexCoord;
+
+uniform float LowThreshold;
+uniform float HighThreshold;
+
+out vec4 FragColor;
+
+vec3 ads(int lightIndex)
+{
+    vec3 s = normalize(Lights[lightIndex].Position.xyz - Position);
+    vec3 v = normalize(vec3(-Position));
+    vec3 h = normalize(v + s);
+    vec3 n = normalize(Normal);
+    
+    vec3 diffuse = Lights[lightIndex].Intensity.xyz * Material.Kd.xyz * max(0.0, dot(s, n));
+    vec3 spec = Lights[lightIndex].Intensity.xyz * Material.Ks.xyz * pow(max(0.0, dot(h, n)), Material.Shininess);
+
+    return diffuse + spec;
+}
+
+void main()
+{
+    // Get the noise value at TexCoord
+    vec4 noise = texture(NoiseTexture, TexCoord);
+
+    // If the value is outside the threshold, discard
+    if(noise.a < LowThreshold) discard;
+    if(noise.a > HighThreshold) discard;
+
+    FragColor = vec4(0, 0, 0, 1);
+    for(int i = 0; i < LightCount; i++)
+    {
+        vec3 diffAndSpec = ads(i);
+        
+        FragColor += vec4(diffAndSpec + Material.Ka.xyz, 1.0);
+    }
+    FragColor.w = 1.0;
+}
