3ab7ccb751cab70b7fe6f12fe097ea55613cacfd
Derrick Canfield <derrick.canfield@gmail.com>
2018-07-19 12:05:04 -0700

    Camera FOV now renders properly

--- DerydocaEngine/Projection.h
+++ DerydocaEngine/Projection.h
@@ -38,7 +38,7 @@ public:
 			m_projectionMatrix = glm::mat4(); //glm::ortho(-m_orthoSize, m_orthoSize, -m_orthoSize, m_orthoSize, m_zNear, m_zFar);
 			break;
 		case ProjectionMode::Perspective:
-			m_projectionMatrix = glm::perspective(m_fov, m_aspect, m_zNear, m_zFar);
+			m_projectionMatrix = glm::perspective(m_fov * (glm::pi<float>() / 180), m_aspect, m_zNear, m_zFar);
 			break;
 		default:
 			m_projectionMatrix = glm::mat4();
