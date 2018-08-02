ddeb17bce56c61e07c63099924f26c1e4fdcfc31
Derrick Canfield <derrick.canfield@gmail.com>
2018-08-02 13:47:12 -0700

    Added word wrap to text renderer

--- DerydocaEngine/TextRenderer.cpp
+++ DerydocaEngine/TextRenderer.cpp
@@ -5,6 +5,30 @@
 const int CARRIAGE_RETURN_CHAR = 13;
 const int DEL_CHAR = 127;
 
+struct LineExtent
+{
+public:
+	LineExtent()
+	{
+		m_start = 0;
+		m_end = 0;
+	}
+
+	LineExtent(int start)
+	{
+		m_start = start;
+		m_end = start;
+	}
+
+	int getStart() const { return m_start; }
+	int getEnd() const { return m_end; }
+	void setStart(int start) { m_start = start; }
+	void setEnd(int end) { m_end = end; }
+private:
+	int m_start;
+	int m_end;
+};
+
 TextRenderer::TextRenderer()
 {
 }
@@ -79,92 +103,123 @@ void TextRenderer::updateText()
 		return;
 	}
 
-	delete[] m_verts;
-	delete[] m_uvs;
-	delete[] m_indices;
-
-	int charCount = 0;
-	for (int i = 0; i < m_text.length(); i++)
-	{
-		// Skip all characters that do not output any quads
-		if (m_text[i] <= 31 || m_text[i] == DEL_CHAR)
-		{
-			continue;
-		}
-		charCount++;
-	}
-
-	int vertCount = charCount * 4;
-	int indicesCount = charCount * 6;
-
-	m_verts = new vec3[vertCount];
-	m_uvs = new vec2[vertCount];
-	m_indices = new unsigned int[indicesCount];
-
 	float xPos = 0.0f;
 	float yPos = 0.0f;
 	float charWidth = 0.1f;
 	float pointScale = 0.01f;
+	float lineHeight = 55.0f;
 
-	// Determine how many chars fit in the next line
-	// Create the mesh data for that
-	// Cause a carriage return
-	// Start again
+	char* filteredString = new char[m_text.length()];
+	vector<LineExtent*> lineExtents;
 
-	int charQuadIndex = 0;
-	for (int i = 0; i < m_text.length(); i++)
+	int filteredStringIndex = 0;
+	float lineWidth = 0.0f;
+
+	// Create a line extent object to be inserted into the lineExtents array when a full line is completed
+	LineExtent* lineExtent = new LineExtent(0);
+	lineExtent->setStart(0);
+
+	for (size_t i = 0; i < m_text.length(); i++)
 	{
-		// Handle carriage returns
-		if (m_text[i] == CARRIAGE_RETURN_CHAR)
+		char c = m_text[i];
+
+		if (c == CARRIAGE_RETURN_CHAR)
 		{
-			yPos -= 50.0f;
-			xPos = 0.0f;
+			// End of line
+			lineExtent->setEnd(filteredStringIndex);
+			lineExtents.push_back(lineExtent);
+			lineExtent = new LineExtent(filteredStringIndex);
+			lineWidth = 0.0f;
 			continue;
 		}
-
-		// Skip all characters that do not output any quads
-		if (m_text[i] <= 31 || m_text[i] == DEL_CHAR)
+		else if (c <= 31 || c == DEL_CHAR)
 		{
+			// Skip char
 			continue;
 		}
 
 		TexturePackerImage img = m_fontFace->getCharData(m_text[i]);
-		Rect rect = img.getTexSheetPosition();
-		float minx = rect.getX();
-		float maxx = rect.getDX();
-		float miny = rect.getY();
-		float maxy = rect.getDY();
+		lineWidth += img.getAdvanceX();
 
-		// Set the UV positions
-		m_uvs[charQuadIndex * 4 + 1] = vec2(minx, maxy);
-		m_uvs[charQuadIndex * 4 + 0] = vec2(minx, miny);
-		m_uvs[charQuadIndex * 4 + 3] = vec2(maxx, miny);
-		m_uvs[charQuadIndex * 4 + 2] = vec2(maxx, maxy);
+		if (lineWidth > m_bounds.x)
+		{
+			// End of line
+			lineExtent->setEnd(filteredStringIndex);
+			lineExtents.push_back(lineExtent);
+			lineExtent = new LineExtent(filteredStringIndex);
+			lineWidth = 0.0f;
+		}
 
-		// Set the indices to draw the two triangles
-		m_indices[charQuadIndex * 6 + 0] = charQuadIndex * 4 + 0;
-		m_indices[charQuadIndex * 6 + 1] = charQuadIndex * 4 + 1;
-		m_indices[charQuadIndex * 6 + 2] = charQuadIndex * 4 + 2;
-		m_indices[charQuadIndex * 6 + 3] = charQuadIndex * 4 + 0;
-		m_indices[charQuadIndex * 6 + 4] = charQuadIndex * 4 + 2;
-		m_indices[charQuadIndex * 6 + 5] = charQuadIndex * 4 + 3;
-
-		// Set the vertex positions
-		float charXMin = xPos + img.getBearingX();
-		float charXMax = xPos + img.getBearingX() + img.getSizeX();
-
-		float charYMax = yPos - img.getSizeY() + img.getBearingY();
-		float charYMin = yPos + img.getBearingY();
-		m_verts[charQuadIndex * 4 + 0] = vec3(charXMin, charYMin, 0);
-		m_verts[charQuadIndex * 4 + 1] = vec3(charXMin, charYMax, 0);
-		m_verts[charQuadIndex * 4 + 2] = vec3(charXMax, charYMax, 0);
-		m_verts[charQuadIndex * 4 + 3] = vec3(charXMax, charYMin, 0);
-
-		xPos += img.getAdvanceX();
-		yPos += img.getAdvanceY();
-
-		charQuadIndex++;
+		filteredString[filteredStringIndex++] = c;
 	}
+
+	// Add the final line extent
+	lineExtent->setEnd(filteredStringIndex);
+	lineExtents.push_back(lineExtent);
+
+	delete[] m_verts;
+	delete[] m_uvs;
+	delete[] m_indices;
+
+	int charCount = lineExtents.back()->getEnd();
+	int vertCount = charCount * 4;
+	int indicesCount = charCount * 6;
+	
+	m_verts = new vec3[vertCount];
+	m_uvs = new vec2[vertCount];
+	m_indices = new unsigned int[indicesCount];
+
+	int charQuadIndex = 0;
+	for (int lineIndex = 0; lineIndex < lineExtents.size(); lineIndex++)
+	{
+		LineExtent* extent = lineExtents[lineIndex];
+		for (int charIndex = extent->getStart(); charIndex < extent->getEnd(); charIndex++)
+		{
+			TexturePackerImage img = m_fontFace->getCharData(filteredString[charIndex]);
+			Rect rect = img.getTexSheetPosition();
+			float minx = rect.getX();
+			float maxx = rect.getDX();
+			float miny = rect.getY();
+			float maxy = rect.getDY();
+
+			// Set the UV positions
+			m_uvs[charQuadIndex * 4 + 1] = vec2(minx, maxy);
+			m_uvs[charQuadIndex * 4 + 0] = vec2(minx, miny);
+			m_uvs[charQuadIndex * 4 + 3] = vec2(maxx, miny);
+			m_uvs[charQuadIndex * 4 + 2] = vec2(maxx, maxy);
+
+			// Set the indices to draw the two triangles
+			m_indices[charQuadIndex * 6 + 0] = charQuadIndex * 4 + 0;
+			m_indices[charQuadIndex * 6 + 1] = charQuadIndex * 4 + 1;
+			m_indices[charQuadIndex * 6 + 2] = charQuadIndex * 4 + 2;
+			m_indices[charQuadIndex * 6 + 3] = charQuadIndex * 4 + 0;
+			m_indices[charQuadIndex * 6 + 4] = charQuadIndex * 4 + 2;
+			m_indices[charQuadIndex * 6 + 5] = charQuadIndex * 4 + 3;
+
+			// Set the vertex positions
+			float charXMin = xPos + img.getBearingX();
+			float charXMax = xPos + img.getBearingX() + img.getSizeX();
+
+			float charYMax = yPos - img.getSizeY() + img.getBearingY();
+			float charYMin = yPos + img.getBearingY();
+			m_verts[charQuadIndex * 4 + 0] = vec3(charXMin, charYMin, 0);
+			m_verts[charQuadIndex * 4 + 1] = vec3(charXMin, charYMax, 0);
+			m_verts[charQuadIndex * 4 + 2] = vec3(charXMax, charYMax, 0);
+			m_verts[charQuadIndex * 4 + 3] = vec3(charXMax, charYMin, 0);
+
+			xPos += img.getAdvanceX();
+			yPos += img.getAdvanceY();
+
+			charQuadIndex++;
+		}
+
+		xPos = 0.0f;
+		yPos -= lineHeight;
+
+		delete extent;
+	}
+
+	delete[] filteredString;
 
 	m_mesh.load(vertCount, m_verts, nullptr, m_uvs, m_indices, indicesCount);
 	m_meshRenderer->setMesh(&m_mesh);
