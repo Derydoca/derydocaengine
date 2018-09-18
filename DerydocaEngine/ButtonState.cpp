78f4d2493ab4f2661762efb9052ad45c767555e1
Derrick Canfield <derrick.canfield@gmail.com>
2018-09-18 15:05:40 -0700

    Namespaced ButtonState

--- DerydocaEngine/ButtonState.cpp
+++ DerydocaEngine/ButtonState.cpp
@@ -1,19 +1,24 @@
 #include "ButtonState.h"
 
-ButtonState::ButtonState(unsigned int const& initialState) :
-	m_currentState(initialState),
-	m_downStates(0),
-	m_upStates(0)
+namespace DerydocaEngine::Input
 {
-}
 
-ButtonState::~ButtonState()
-{
-}
+	ButtonState::ButtonState(unsigned int const& initialState) :
+		m_currentState(initialState),
+		m_downStates(0),
+		m_upStates(0)
+	{
+	}
 
-void ButtonState::update(unsigned int const& currentState)
-{
-	unsigned int changedStates = currentState ^ m_currentState;
-	m_downStates = changedStates & currentState;
-	m_upStates = changedStates & (~currentState);
+	ButtonState::~ButtonState()
+	{
+	}
+
+	void ButtonState::update(unsigned int const& currentState)
+	{
+		unsigned int changedStates = currentState ^ m_currentState;
+		m_downStates = changedStates & currentState;
+		m_upStates = changedStates & (~currentState);
+	}
+
 }
