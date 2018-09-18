2348fefee9e24a2ee76171023b9da7faaa6a37ae
Derrick Canfield <derrick.canfield@gmail.com>
2018-09-18 15:27:22 -0700

    Namespaced Key

--- DerydocaEngine/Key.h
+++ DerydocaEngine/Key.h
@@ -1,32 +1,36 @@
 #pragma once
 #include "sdl2\SDL.h"
 
-struct Key
+namespace DerydocaEngine::Input
 {
-public:
-	Key()
+
+	struct Key
 	{
-		m_isDown = false;
-		m_downTime = 0;
-	}
-	~Key() {}
-
-	inline bool isDown() const { return m_isDown; }
-	void setState(bool const& isDown, unsigned long const& tick) {
-
-		if (m_prevDownState != isDown)
+	public:
+		Key()
 		{
-			m_stateChangeTick = tick;
+			m_isDown = false;
+			m_downTime = 0;
 		}
-		m_prevDownState = m_isDown;
-		m_isDown = isDown;
-	}
+		~Key() {}
 
-	unsigned long getStateChangeTick() { return m_stateChangeTick; }
-private:
-	unsigned int m_downTime;
-	bool m_isDown;
-	bool m_prevDownState;
-	unsigned long m_stateChangeTick = 0;
-};
+		inline bool isDown() const { return m_isDown; }
+		void setState(bool const& isDown, unsigned long const& tick) {
 
+			if (m_prevDownState != isDown)
+			{
+				m_stateChangeTick = tick;
+			}
+			m_prevDownState = m_isDown;
+			m_isDown = isDown;
+		}
+
+		unsigned long getStateChangeTick() { return m_stateChangeTick; }
+	private:
+		unsigned int m_downTime;
+		bool m_isDown;
+		bool m_prevDownState;
+		unsigned long m_stateChangeTick = 0;
+	};
+
+}
