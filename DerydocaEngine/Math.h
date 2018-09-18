ec0930b64a2d04097b1fbe5fb1e45c78feb71c52
Derrick Canfield <derrick.canfield@gmail.com>
2018-09-18 14:24:47 -0700

    Removed Math.cpp and Math.h since it is not used

--- DerydocaEngine/Math.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "glm\glm.hpp"
-
-unsigned int MSB = ((unsigned int)-1 >> 1);
-
-bool floatsShareSign(float const& leftValue, float const& rightValue);
-glm::vec3 getNormal(glm::vec3 const& vector1, glm::vec3 const& vector2);
