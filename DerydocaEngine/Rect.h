b4bfafa5003bf94a69544747c94be8adb0a53e93
Derrick Canfield <derrick.canfield@gmail.com>
2018-09-18 14:26:25 -0700

    Namespaced Rect

--- DerydocaEngine/Rect.h
+++ DerydocaEngine/Rect.h
@@ -1,39 +1,44 @@
 #pragma once
 
-struct Rect
+namespace DerydocaEngine
 {
-public:
-	Rect()
-	{
-		m_x = 0.0f;
-		m_y = 0.0f;
-		m_dx = 1.0f;
-		m_dy = 1.0f;
-	}
 
-	Rect(float const& x, float const& y, float const& dx, float const& dy) :
-		m_x(x),
-		m_y(y),
-		m_dx(dx),
-		m_dy(dy)
+	struct Rect
 	{
-	}
+	public:
+		Rect()
+		{
+			m_x = 0.0f;
+			m_y = 0.0f;
+			m_dx = 1.0f;
+			m_dy = 1.0f;
+		}
 
-	float getX() const { return m_x; }
-	float getY() const { return m_y; }
-	float getDX() const { return m_dx; }
-	float getDY() const { return m_dy; }
-	void set(float const& x, float const& y, float const& dx, float const& dy)
-	{
-		m_x = x;
-		m_y = y;
-		m_dx = dx;
-		m_dy = dy;
-	}
-	void setX(float const& x) { m_x = x; }
-	void setY(float const& y) { m_y = y; }
-	void setDX(float const& w) { m_dx = w; }
-	void setDY(float const& h) { m_dy = h; }
-private:
-	float m_x, m_y, m_dx, m_dy;
-};
+		Rect(float const& x, float const& y, float const& dx, float const& dy) :
+			m_x(x),
+			m_y(y),
+			m_dx(dx),
+			m_dy(dy)
+		{
+		}
+
+		float getX() const { return m_x; }
+		float getY() const { return m_y; }
+		float getDX() const { return m_dx; }
+		float getDY() const { return m_dy; }
+		void set(float const& x, float const& y, float const& dx, float const& dy)
+		{
+			m_x = x;
+			m_y = y;
+			m_dx = dx;
+			m_dy = dy;
+		}
+		void setX(float const& x) { m_x = x; }
+		void setY(float const& y) { m_y = y; }
+		void setDX(float const& w) { m_dx = w; }
+		void setDY(float const& h) { m_dy = h; }
+	private:
+		float m_x, m_y, m_dx, m_dy;
+	};
+
+}
