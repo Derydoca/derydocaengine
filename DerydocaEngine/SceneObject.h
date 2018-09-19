e04c775899c0e54436fe0cb1d05fe568218d3f3b
Derrick Canfield <derrick.canfield@gmail.com>
2018-09-19 10:37:55 -0700

    Namespaced SceneObject

--- DerydocaEngine/SceneObject.h
+++ DerydocaEngine/SceneObject.h
@@ -3,27 +3,32 @@
 #include <boost\uuid\uuid.hpp>
 #include "GameObject.h"
 
-class SceneObject
+namespace DerydocaEngine::Scenes
 {
-public:
-	SceneObject(boost::uuids::uuid const& id, std::string const& type, YAML::Node const& propertiesNode) :
-		m_id(id),
-		m_type(type),
-		m_properties(propertiesNode)
+
+	class SceneObject
 	{
-	}
+	public:
+		SceneObject(boost::uuids::uuid const& id, std::string const& type, YAML::Node const& propertiesNode) :
+			m_id(id),
+			m_type(type),
+			m_properties(propertiesNode)
+		{
+		}
 
-	~SceneObject() {}
+		~SceneObject() {}
 
-	boost::uuids::uuid getId() const { return m_id; }
-	std::string getType() const { return m_type; }
-	YAML::Node getProperties() const { return m_properties; }
-	bool isObjectCreated() const { return m_objectReference != 0; }
-	DerydocaEngine::GameObject* getGameObject() const { return m_objectReference; }
-	void setObjectReference(DerydocaEngine::GameObject* const& reference) { m_objectReference = reference; }
-private:
-	boost::uuids::uuid m_id;
-	std::string m_type;
-	YAML::Node m_properties;
-	DerydocaEngine::GameObject* m_objectReference;
-};
+		boost::uuids::uuid getId() const { return m_id; }
+		std::string getType() const { return m_type; }
+		YAML::Node getProperties() const { return m_properties; }
+		bool isObjectCreated() const { return m_objectReference != 0; }
+		DerydocaEngine::GameObject* getGameObject() const { return m_objectReference; }
+		void setObjectReference(DerydocaEngine::GameObject* const& reference) { m_objectReference = reference; }
+	private:
+		boost::uuids::uuid m_id;
+		std::string m_type;
+		YAML::Node m_properties;
+		DerydocaEngine::GameObject* m_objectReference;
+	};
+
+}
