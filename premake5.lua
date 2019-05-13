9a91fe763fb12db9d3a7f178f802e8d4c635567e
Derrick Canfield <derrick.canfield@gmail.com>
2019-05-13 15:39:05 -0700

    Created premake definition for DerydocaEngine.Components project

--- premake5.lua
+++ premake5.lua
@@ -11,16 +11,16 @@ workspace "DerydocaEngine"
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
 -- SDL checks - Yes (/sdl): This option is missing from Premake but existed in the manually created project
-project "DerydocaEngine"
-    location "DerydocaEngine"
+project "DerydocaEngine.Components"
+    location "DerydocaEngine.Components"
     kind "StaticLib"
     language "C++"
 
     targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-    pchheader "EnginePch.h"
-    pchsource "%{prj.location}/src/EnginePch.cpp"
+    pchheader "EngineComponentsPch.h"
+    pchsource "%{prj.location}/src/EngineComponentsPch.cpp"
     staticruntime "off"
 
     flags
@@ -38,7 +38,7 @@ project "DerydocaEngine"
     {
         "C:/local/boost_1_68_0",
         "D:/local/boost_1_68_0",
-        "%{wks.location}/%{wks.name}.Components/src",
+        "%{wks.location}/DerydocaEngine/src",
         "%{prj.location}/src",
         "%{wks.location}/include"
     }
@@ -56,11 +56,6 @@ project "DerydocaEngine"
             "_SILENCE_CXX17_ITERATOR_BASE_CLASS_DEPRECATION_WARNING"
         }
 
-        -- postbuildcommands
-        -- {
-        --     ("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/DerydocaEngine.Editor.UI")
-        -- }
-
     filter "configurations:Debug"
         defines "DD_DEBUG"
         symbols "On"
@@ -72,6 +67,68 @@ project "DerydocaEngine"
     filter "configurations:Dist"
         defines "DD_DIST"
         symbols "On"
+
+        project "DerydocaEngine"
+        location "DerydocaEngine"
+        kind "StaticLib"
+        language "C++"
+    
+        targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+        objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+    
+        pchheader "EnginePch.h"
+        pchsource "%{prj.location}/src/EnginePch.cpp"
+        staticruntime "off"
+    
+        flags
+        {
+            "MultiProcessorCompile"
+        }
+    
+        files
+        {
+            "%{prj.name}/src/**.h",
+            "%{prj.name}/src/**.cpp"
+        }
+    
+        includedirs
+        {
+            "C:/local/boost_1_68_0",
+            "D:/local/boost_1_68_0",
+            "%{wks.location}/%{wks.name}.Components/src",
+            "%{prj.location}/src",
+            "%{wks.location}/include"
+        }
+    
+        filter "system:windows"
+            cppdialect "C++17"
+            staticruntime "On"
+            systemversion "latest"
+    
+            defines
+            {
+                "OPENGL=1",
+                "_CRT_SECURE_NO_WARNINGS",
+                "YAML_DECLARE_STATIC",
+                "_SILENCE_CXX17_ITERATOR_BASE_CLASS_DEPRECATION_WARNING"
+            }
+    
+            -- postbuildcommands
+            -- {
+            --     ("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/DerydocaEngine.Editor.UI")
+            -- }
+    
+        filter "configurations:Debug"
+            defines "DD_DEBUG"
+            symbols "On"
+    
+        filter "configurations:Release"
+            defines "DD_RELEASE"
+            symbols "On"
+    
+        filter "configurations:Dist"
+            defines "DD_DIST"
+            symbols "On"
 
 project "DerydocaEngine.Editor.UI"
     location "DerydocaEngine.Editor.UI"
