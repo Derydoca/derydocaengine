5b18f90a97597918efc4e60df1e25bdc535aa587
Derrick Canfield <derrick.canfield@gmail.com>
2019-05-13 15:42:10 -0700

    Fixed indentation issue

--- premake5.lua
+++ premake5.lua
@@ -68,67 +68,67 @@ project "DerydocaEngine.Components"
         defines "DD_DIST"
         symbols "On"
 
-        project "DerydocaEngine"
-        location "DerydocaEngine"
-        kind "StaticLib"
-        language "C++"
-    
-        targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-        objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
-    
-        pchheader "EnginePch.h"
-        pchsource "%{prj.location}/src/EnginePch.cpp"
-        staticruntime "off"
-    
-        flags
+project "DerydocaEngine"
+    location "DerydocaEngine"
+    kind "StaticLib"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    pchheader "EnginePch.h"
+    pchsource "%{prj.location}/src/EnginePch.cpp"
+    staticruntime "off"
+
+    flags
+    {
+        "MultiProcessorCompile"
+    }
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs
+    {
+        "C:/local/boost_1_68_0",
+        "D:/local/boost_1_68_0",
+        "%{wks.location}/%{wks.name}.Components/src",
+        "%{prj.location}/src",
+        "%{wks.location}/include"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines
         {
-            "MultiProcessorCompile"
+            "OPENGL=1",
+            "_CRT_SECURE_NO_WARNINGS",
+            "YAML_DECLARE_STATIC",
+            "_SILENCE_CXX17_ITERATOR_BASE_CLASS_DEPRECATION_WARNING"
         }
-    
-        files
-        {
-            "%{prj.name}/src/**.h",
-            "%{prj.name}/src/**.cpp"
-        }
-    
-        includedirs
-        {
-            "C:/local/boost_1_68_0",
-            "D:/local/boost_1_68_0",
-            "%{wks.location}/%{wks.name}.Components/src",
-            "%{prj.location}/src",
-            "%{wks.location}/include"
-        }
-    
-        filter "system:windows"
-            cppdialect "C++17"
-            staticruntime "On"
-            systemversion "latest"
-    
-            defines
-            {
-                "OPENGL=1",
-                "_CRT_SECURE_NO_WARNINGS",
-                "YAML_DECLARE_STATIC",
-                "_SILENCE_CXX17_ITERATOR_BASE_CLASS_DEPRECATION_WARNING"
-            }
-    
-            -- postbuildcommands
-            -- {
-            --     ("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/DerydocaEngine.Editor.UI")
-            -- }
-    
-        filter "configurations:Debug"
-            defines "DD_DEBUG"
-            symbols "On"
-    
-        filter "configurations:Release"
-            defines "DD_RELEASE"
-            symbols "On"
-    
-        filter "configurations:Dist"
-            defines "DD_DIST"
-            symbols "On"
+
+        -- postbuildcommands
+        -- {
+        --     ("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/DerydocaEngine.Editor.UI")
+        -- }
+
+    filter "configurations:Debug"
+        defines "DD_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "DD_RELEASE"
+        symbols "On"
+
+    filter "configurations:Dist"
+        defines "DD_DIST"
+        symbols "On"
 
 project "DerydocaEngine.Editor.UI"
     location "DerydocaEngine.Editor.UI"
