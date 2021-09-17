a0573cca320f9042b76522c0a748f9e9443bc219
Derrick <derrick.canfield@gmail.com>
2021-09-17 09:48:22 -0700

    Replaced boost fs with std fs in ShaderrResource.cpp

--- DerydocaEngine/src/Resources/ShaderResource.cpp
+++ DerydocaEngine/src/Resources/ShaderResource.cpp
@@ -1,23 +1,20 @@
 #include "EnginePch.h"
 #include "ShaderResource.h"
 #include "Rendering\ShaderLibrary.h"
-#include <boost\filesystem.hpp>
-
-namespace fs = boost::filesystem;
 
 void DerydocaEngine::Resources::ShaderResource::postLoadInitialize()
 {
-	fs::path vertexShaderPath(getVertexShaderLocation());
+	std::filesystem::path vertexShaderPath(getVertexShaderLocation());
 
 	// Get the path that this exists for
-	fs::path shaderDirectory = vertexShaderPath.parent_path();
+	std::filesystem::path shaderDirectory = vertexShaderPath.parent_path();
 
 	// Remove the extension from the file name so we can append to all other shader types
-	fs::path rawShaderName = change_extension(vertexShaderPath, "");
+	std::filesystem::path rawShaderName = vertexShaderPath.replace_extension();
 	setRawShaderName(rawShaderName.string());
 
 	// If a fragment shader exists
-	fs::path fragmentShaderPath = rawShaderName.append(".fs");
+	std::filesystem::path fragmentShaderPath = rawShaderName.append(".fs");
 	if (exists(fragmentShaderPath))
 	{
 		// Set the path to the fragment shader
@@ -25,7 +22,7 @@ void DerydocaEngine::Resources::ShaderResource::postLoadInitialize()
 	}
 
 	// If a geometry shader exists
-	fs::path geometryShaderPath = rawShaderName.append(".gs");
+	std::filesystem::path geometryShaderPath = rawShaderName.append(".gs");
 	if (exists(geometryShaderPath))
 	{
 		// Set the path to the geometry shader
