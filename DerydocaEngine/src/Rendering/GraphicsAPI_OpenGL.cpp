a5aaded3077015fcf9ce83048e29390cc95adf5a
Derrick <derrick.canfield@gmail.com>
2021-10-31 09:17:29 -0700

    Moved GLError to the GAPI implementation

--- DerydocaEngine/src/Rendering/GraphicsAPI_OpenGL.cpp
+++ DerydocaEngine/src/Rendering/GraphicsAPI_OpenGL.cpp
@@ -4,11 +4,43 @@
 
 #include <GL/glew.h>
 #include "GraphicsAPI.h"
-#include "Debug\GLError.h"
 #include "Shader.h"
+
+#ifdef _DEBUG
+#define GL_CHECK(stmt) stmt; CheckOpenGLError(#stmt, __FILE__, __LINE__);
+#else
+#define GL_CHECK(stmt) stmt
+#endif
 
 namespace DerydocaEngine::Rendering
 {
+	const char* GLErrorToString(unsigned int err)
+	{
+		switch (err)
+		{
+		case GL_NO_ERROR:                      return "GL_NO_ERROR";
+		case GL_INVALID_ENUM:                  return "GL_INVALID_ENUM";
+		case GL_INVALID_VALUE:                 return "GL_INVALID_VALUE";
+		case GL_INVALID_OPERATION:             return "GL_INVALID_OPERATION";
+		case GL_STACK_OVERFLOW:                return "GL_STACK_OVERFLOW";
+		case GL_STACK_UNDERFLOW:               return "GL_STACK_UNDERFLOW";
+		case GL_OUT_OF_MEMORY:                 return "GL_OUT_OF_MEMORY";
+		case 0x8031: /* not core */            return "GL_TABLE_TOO_LARGE_EXT";
+		case 0x8065: /* not core */            return "GL_TEXTURE_TOO_LARGE_EXT";
+		case GL_INVALID_FRAMEBUFFER_OPERATION: return "GL_INVALID_FRAMEBUFFER_OPERATION";
+		default:
+			assert(!"Unhandled GL error code");
+			return NULL;
+		}
+	}
+
+	void CheckOpenGLError(char* const& stmt, char* const& fname, int const& line) {
+		GLenum err = glGetError();
+		if (err != GL_NO_ERROR) {
+			D_LOG_ERROR("GL ERROR: {}\n    {} : {}", GLErrorToString(err), fname, line);
+		}
+	}
+
 	int GraphicsAPI::USAGE_PATTERN_DYNAMIC_DRAW = GL_DYNAMIC_DRAW;
 
 	void GraphicsAPI::init()
