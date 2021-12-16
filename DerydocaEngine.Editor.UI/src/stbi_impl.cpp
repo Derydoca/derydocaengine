9f1458061716b01903d858362149321228a8e74e
Derrick <derrick.canfield@gmail.com>
2021-12-15 16:04:38 -0800

    Removed lib, vendor, and include dirs

--- DerydocaEngine.Editor.UI/src/stbi_impl.cpp
+++
@@ -1,7 +0,0 @@
-#include "EditorPch.h"
-
-#define STB_IMAGE_IMPLEMENTATION
-#include "vendor\stb_image.h"
-#define STBI_MSC_SECURE_CRT
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "vendor\stb_image_write.h"
