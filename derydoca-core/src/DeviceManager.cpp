b81bd53753fe4ea56fdf4fff7784985efd2b531c
Derrick Canfield <derrick.canfield@gmail.com>
2022-10-27 22:00:07 -0700

    DeviceManager::Create() now returns a pointer to the correct object

--- derydoca-core/src/DeviceManager.cpp
+++ derydoca-core/src/DeviceManager.cpp
@@ -1,9 +1,29 @@
 #include "Derydoca/DeviceManager.h"
 #include "Derydoca/Logging/Log.h"
 
+#include "Derydoca/Logging/Log.h"
+#ifdef USE_DX12
+#include "Derydoca/Rendering/DX12/DeviceManagerDX12.h"
+#endif
+#ifdef USE_VULKAN
+#include "Derydoca/Rendering/VK/DeviceManagerVK.h"
+#endif
+
 namespace DerydocaEngine {
 	DeviceManager* DerydocaEngine::DeviceManager::Create(const RenderingAPI renderingAPI)
 	{
-		return nullptr;
+		switch (renderingAPI)
+		{
+#ifdef USE_DX12
+		case RenderingAPI::Direct3D12:
+			return new Rendering::DeviceManagerDX12();
+#endif
+#ifdef USE_VULKAN
+		case RenderingAPI::Vulkan:
+			return new Rendering::DeviceManagerVK();
+#endif
+		default:
+			return nullptr;
+		}
 	}
 }
